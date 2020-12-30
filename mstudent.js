77815b3410598ebeb73b60d2bd8929a41049794e
vijayabalan <vijaynationallionz@gmail.com>
2020-12-30 20:47:36 +0530

    Delete mstudent.js

--- mstudent.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const StudentSchema = mongoose.Schema({
-	Name:{type: String},
-    Address:{type: String}
-});
-
-
-module.exports = { Student: mongoose.model('student', StudentSchema) };
